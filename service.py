52c4b3f764fd712a76ea3554d0d1f78080a99ab5
Luis Valdes <luischivaldes@gmail.com>
2018-10-18 03:56:21 -0300

    Cleaning up and making the demo more functional
    Signed-off-by: Luis Valdes <luischivaldes@gmail.com>

--- service.py
+++ service.py
@@ -5,49 +5,51 @@ import os
 import subprocess
 import boto3
 
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
 
-def downloadFromS3(strBucket,strKey,strFile):
-    s3_client = boto3.client('s3')
-    s3_client.download_file(strBucket, strKey, strFile)
+def downloadFromS3(strBucket, strKey, strFile):
+  s3_client = boto3.client('s3')
+  s3_client.download_file(strBucket, strKey, strFile)
 
-
+def uploadToS3(strFile, strBucket, strKey):
+  s3_client = boto3.client('s3')
+  s3_client.upload_file(strFile, strBucket, strKey, ExtraArgs={'ACL':'public-read'})
 
 def handler(event, context):
-    try:
-        imgfilepath = '/tmp/inputimage.jpg'
-        if ('imagelink' in event):
-          urllib.urlretrieve(event['imagelink'], imgfilepath)
-        else:
-          strBucket = 'myqueuecounter'
-          strKey = 'darknet/street.jpg'
-          downloadFromS3(strBucket,strKey,imgfilepath)
-        print(imgfilepath)
-        
-        strBucket = 'myqueuecounter'
-        strKey = 'darknet/yolov3.weights'
-        strWeightFile = '/tmp/yolov3.weights'
-        downloadFromS3(strBucket,strKey,strWeightFile)  
-        print(strWeightFile)
+  try:
+    # Download yolo weights
+    strBucket = 'darknet-images-latinoware'
+    strKey = 'darknet/yolov3.weights'
+    strWeightFile = '/tmp/yolov3.weights'
+    downloadFromS3(strBucket, strKey, strWeightFile)
 
-        command = './darknet detect cfg/yolov3.cfg {} {}'.format(
-            strWeightFile,
-            imgfilepath
-        )
-        print(command)
+    for imagePath in map(lambda r: r['s3']['object']['key'], event['Records']):
+      # Download image from bucket
+      imageFile = imagePath.split('/').pop()
+      imageFilePath = '/tmp/{}'.format(imageFile)
+      downloadFromS3(strBucket, imagePath, imageFilePath)
+      
+      predictionsPath = '/tmp/predictions.png'
+      command = './darknet detect cfg/yolov3.cfg {} {} -out /tmp/predictions'.format(
+          strWeightFile,
+          imageFilePath
+      )
+      print(command)
 
-        try:
-            print('Start')
-            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-            print('Finish')
-            print(output)
-        except subprocess.CalledProcessError as e:
-            print('Error')
-            print(e.output)
-    except Exception as e:
-        print('Error e')
-        print(e)
-        raise e
-    return 0 
+      try:
+        print('Start')
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+        print('Finish')
+        # upload predictions to bucket
+        predictionsFile = '{}.png'.format(os.path.splitext(imageFile)[0])
+        predictionsKey = 'predictions/{}'.format(predictionsFile)
+        uploadToS3(predictionsPath, strBucket, predictionsKey)
+      except subprocess.CalledProcessError as e:
+        print('Error')
+        print(e.output)
+  except Exception as e:
+    print('Error')
+    print(e)
+    raise e
+  return 0 
